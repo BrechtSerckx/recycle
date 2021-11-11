30e94e5e01114518737416783dbfde0b97badd37
Brecht Serckx <services+git@brechtserckx.be>
2021-11-11 17:44:10 +0100

    Enable date range inputs depending on date range type radios

--- www/index.js
+++ www/index.js
@@ -17,9 +17,41 @@ function attachFormSubmit(form) {
     });
 }
 
+// Enable a date range type radio input and its corresponding date range inputs.
+function enableDateRangeType(radios, thisRadio) {
+    thisRadio.checked = true;
+    // for all other radio inputs, disable the corresponding inputs
+    for (thatRadio of radios) {
+        if (thatRadio.id != thisRadio.id) {
+
+            for (input of document.getElementsByClassName(thatRadio.id)) {
+                input.disabled = true;
+            }
+        } else {}
+    }
+    // enable the corresponding inputs for this radio
+    for (input of document.getElementsByClassName(thisRadio.id)) {
+        input.disabled = false;
+    }
+
+}
+
+// Attach a function to the `date_range_type` radio inputs so their respective
+// input fields get enabled or disabled based on the radio input.
+function attachDateRangeTypeFields(radios) {
+    for (const radio of radios) {
+        radio.onclick = () => {
+            enableDateRangeType(radios, radio);
+        };
+    }
+}
 
 function main() {
     attachFormSubmit(document.getElementById("recycleForm"));
+
+    const radios = document.getElementsByName("date_range_type");
+    attachDateRangeTypeFields(radios);
+    enableDateRangeType(radios, document.getElementById("date_range_type_rel"));
 };
 
 main();
