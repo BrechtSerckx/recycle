7d6d3572d9da90bc2172f88273237e7fc5f66b9b
Brecht Serckx <services+git@brechtserckx.be>
2021-11-21 22:01:01 +0100

    Submit button section in form

--- www/index.js
+++ www/index.js
@@ -1,5 +1,25 @@
 
+// Attach a custom submit function to the form.
+function attachFormSubmit(form) {
+
+    // Function to execute when the form is submitted.
+    function submit() {
+        // get form data
+        const formData = new FormData(form);
+        for (var entry of formData.entries()) {
+            console.log(entry[0], entry[1]);
+        }
+    }
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        submit();
+    });
+}
+
+
 function main() {
+    attachFormSubmit(document.getElementById("recycleForm"));
 };
 
 main();
