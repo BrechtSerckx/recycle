e98a51c5bc4e218e8e95534d926f2e9f9455d2f3
Brecht Serckx <services+git@brechtserckx.be>
2021-11-21 21:59:19 +0100

    Initial webform
    Load and run JavaScript file deferred

--- shell.nix
+++ shell.nix
@@ -1,10 +1,11 @@
 let pkgs = import ./pkgs.nix;
 in pkgs.haskellPackages.shellFor {
   packages = p: [ p.recycle ];
-  buildInputs = with pkgs; [
-    haskellPackages.cabal-install
-    haskellPackages.brittany
-    haskellPackages.ghcid
-    haskellPackages.hlint
-  ];
+  buildInputs = with pkgs;
+    [
+      haskellPackages.cabal-install
+      haskellPackages.brittany
+      haskellPackages.ghcid
+      haskellPackages.hlint
+    ] ++ (with pkgs.nodePackages; [ js-beautify eslint ]);
 }
