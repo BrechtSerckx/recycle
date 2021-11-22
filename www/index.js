c8c3cabbadf66ffc703818a2f97834f38026db81
Brecht Serckx <services+git@brechtserckx.be>
2021-11-22 21:07:12 +0100

    Add debounce

--- www/index.js
+++ www/index.js
@@ -39,6 +39,44 @@ function attachFormSubmit(form, permalink, openLink, downloadLink) {
     });
 }
 
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+//
+// See: https://stackoverflow.com/questions/24004791/can-someone-explain-the-debounce-function-in-javascript
+function debounce(func, wait) {
+    // 'private' variable for instance
+    // The returned function will be able to reference this due to closure.
+    // Each call to the returned function will share this common timer.
+    var timeout;
+
+    // Calling debounce returns a new anonymous function
+    return function() {
+        // reference the context and args for the setTimeout function
+        var context = this,
+            args = arguments;
+
+        // This is the basic debounce behaviour where you can call this 
+        //   function several times, but it will only execute once 
+        //   [before or after imposing a delay]. 
+        //   Each time the returned function is called, the timer starts over.
+        clearTimeout(timeout);
+
+        // Set the new timeout
+        timeout = setTimeout(function() {
+
+            // Inside the timeout function, clear the timeout variable
+            // which will let the next execution run when in 'immediate' mode
+            timeout = null;
+            // Call the original function with apply
+            // apply lets you define the 'this' object as well as the arguments 
+            //    (both captured before setTimeout)
+            func.apply(context, args);
+        }, wait);
+    };
+}
+
 function getLangCode() {
     var checkedRadio = Array.from(document.querySelectorAll("[name=lc]")).filter(x => x.checked)[0];
     console.log(checkedRadio.value);
@@ -93,7 +131,7 @@ function removeChildren(node) {
 }
 
 function createAutoCompleter(input, container, getData, mkLabel, withResult, threshold = 3) {
-    input.oninput = function() {
+    input.oninput = debounce(function() {
         const q = this.value;
         removeChildren(container);
         if (q.length >= threshold) {
@@ -113,7 +151,7 @@ function createAutoCompleter(input, container, getData, mkLabel, withResult, thr
                 }
             });
         }
-    };
+    }, 250);
 };
 
 function createZipcodeCompleter(input, container, target, streetQInput, submitButton) {
