24ef4e679e4f29ebafd4b2a3f26b8c8bb4208577
Brecht Serckx <services+git@brechtserckx.be>
2022-11-25 18:17:15 +0100

    Replace react example by react-hook-form example

--- recycle-ics/frontend/src/App.tsx
+++ recycle-ics/frontend/src/App.tsx
@@ -1,18 +1,33 @@
-import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-function App() {
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+};
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
+
   return (
-    <div>
-      <header>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input defaultValue="test" {...register("example")} />
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("exampleRequired", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
   );
 }
-
-export default App;
