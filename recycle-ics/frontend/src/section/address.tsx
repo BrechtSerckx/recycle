6820ce8170ddbe77d59b983a03b336f220f09bb2
Brecht Serckx <services+git@brechtserckx.be>
2022-11-26 12:57:57 +0100

    Split address section in subcomponents

--- recycle-ics/frontend/src/section/address.tsx
+++ recycle-ics/frontend/src/section/address.tsx
@@ -1,18 +1,10 @@
 import Autocompleter from "../Autocompleter";
 import * as React from "react";
 
-export default function AddressSection({
-  register,
-  setValue,
-  errors,
-  watch,
-}: any) {
+function ZipcodeSection({ register, watch, setValue, errors, children }: any) {
   const [zipcodeSelected, setZipcodeSelected] = React.useState(false);
-  const [streetSelected, setStreetSelected] = React.useState(false);
   return (
     <>
-      <h3>Address</h3>
-      <p>Waste collections are specific to your address.</p>
       <h4>Zip code</h4>
       <div>
         <p>Search for your city and choose the correct city from the list.</p>
@@ -60,72 +52,114 @@ export default function AddressSection({
             />
             {errors.zipcode_id && <span>{errors.zipcode_id.message}</span>}
           </label>
-
-          <h4>Street</h4>
-
-          <div>
-            <p>
-              Search for your street and choose the correct street from the
-              list.
-            </p>
-          </div>
-          <label>
-            Search your street:
-            <input
-              type="text"
-              placeholder="Grote Markt"
-              {...register("street_q")}
-            />
-          </label>
-          <Autocompleter<string, string>
-            query={watch("street_q")}
-            fetchValues={(query) => ["foo", "bar", "baz"]}
-            onSelect={(street) => {
-              setValue("street_id", street);
-              setValue("street_name", street);
-              setStreetSelected(true);
-            }}
-            displayValue={(v) => <span>{v}</span>}
-          />
-          {streetSelected && (
-            <>
-              <label>
-                Street name:
-                <input
-                  type="text"
-                  readOnly
-                  {...register("street_name", { required: true })}
-                />
-              </label>
-              <label>
-                Street ID:
-                <input
-                  type="text"
-                  readOnly
-                  {...register("street_id", {
-                    required: "Please select street",
-                    onChange: () => setStreetSelected(false),
-                  })}
-                />
-                {errors.street_id && <span>{errors.street_id.message}</span>}
-              </label>
-
-              <div>
-                <h4>House number</h4>
-                <p>Enter your house number.</p>
-                <label>
-                  House number:
-                  <input
-                    type="number"
-                    placeholder="1"
-                    {...register("house_number", { required: true })}
-                  />
-                </label>
-              </div>
-            </>
-          )}
+          {children}
         </>
       )}
+    </>
+  );
+}
+
+function StreetSection({ register, watch, setValue, errors, children }: any) {
+  const [streetSelected, setStreetSelected] = React.useState(false);
+  return (
+    <>
+      <h4>Street</h4>
+
+      <div>
+        <p>
+          Search for your street and choose the correct street from the list.
+        </p>
+      </div>
+      <label>
+        Search your street:
+        <input
+          type="text"
+          placeholder="Grote Markt"
+          {...register("street_q")}
+        />
+      </label>
+      <Autocompleter<string, string>
+        query={watch("street_q")}
+        fetchValues={(query) => ["foo", "bar", "baz"]}
+        onSelect={(street) => {
+          setValue("street_id", street);
+          setValue("street_name", street);
+          setStreetSelected(true);
+        }}
+        displayValue={(v) => <span>{v}</span>}
+      />
+      {streetSelected && (
+        <>
+          <label>
+            Street name:
+            <input
+              type="text"
+              readOnly
+              {...register("street_name", { required: true })}
+            />
+          </label>
+          <label>
+            Street ID:
+            <input
+              type="text"
+              readOnly
+              {...register("street_id", {
+                required: "Please select street",
+                onChange: () => setStreetSelected(false),
+              })}
+            />
+            {errors.street_id && <span>{errors.street_id.message}</span>}
+          </label>
+
+          {children}
+        </>
+      )}
+    </>
+  );
+}
+
+function HouseNumberSection({ register }: any) {
+  return (
+    <>
+      <h4>House number</h4>
+      <p>Enter your house number.</p>
+      <label>
+        House number:
+        <input
+          type="number"
+          placeholder="1"
+          {...register("house_number", { required: true })}
+        />
+      </label>
+    </>
+  );
+}
+
+export default function AddressSection({
+  register,
+  setValue,
+  errors,
+  watch,
+}: any) {
+  return (
+    <>
+      <h3>Address</h3>
+      <p>Waste collections are specific to your address.</p>
+      <ZipcodeSection
+        register={register}
+        watch={watch}
+        errors={errors}
+        setValue={setValue}
+      >
+        <StreetSection
+          register={register}
+          watch={watch}
+          errors={errors}
+          setValue={setValue}
+        >
+          <HouseNumberSection register={register} />
+        </StreetSection>
+      </ZipcodeSection>
     </>
   );
 }
