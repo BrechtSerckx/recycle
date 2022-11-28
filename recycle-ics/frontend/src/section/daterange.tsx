5d70f535713381bbc7150f1c03e1e43e909d8219
Brecht Serckx <services+git@brechtserckx.be>
2022-11-28 12:07:12 +0100

    Initial daterange

--- recycle-ics/frontend/src/section/daterange.tsx
+++ recycle-ics/frontend/src/section/daterange.tsx
@@ -1,3 +1,56 @@
+const AbsoluteDateRangeInputs = () => (
+  <div>
+    <label>
+      <input type="radio" name="drt" value="absolute" defaultChecked />
+      Absolute:
+      <p>This will get the waste collections between two dates.</p>
+      <label>
+        From:
+        <input type="date" name="f" required />
+      </label>
+      <label>
+        To:
+        <input type="date" name="t" required />
+      </label>
+    </label>
+  </div>
+);
+const RelativeDateRangeInputs = () => (
+  <div>
+    <label>
+      <input type="radio" name="drt" value="relative" />
+      Relative:
+      <p>
+        This will get the waste collections relative to the current date. This
+        is is particularly useful when auto-importing the waste collections
+        through Google Calendar or Outlook, as it will always give the
+        collections relative to that date. The collections will always be
+        up-to-date like this.
+      </p>
+      <label>
+        Days before:
+        <input type="number" step={1} name="f" required value="-14" disabled />
+      </label>
+      <label>
+        Days after:
+        <input type="number" step={1} name="t" required value="28" disabled />
+      </label>
+    </label>
+  </div>
+);
 export default function DateRangeSection() {
-  return <></>;
+  return (
+    <>
+      <h3>Date range</h3>
+      <p>
+        Choose a start date and an end date for which you want the waste
+        collections.
+      </p>
+      <fieldset>
+        <legend>Date range type</legend>
+        <AbsoluteDateRangeInputs />
+        <RelativeDateRangeInputs />
+      </fieldset>
+    </>
+  );
 }
