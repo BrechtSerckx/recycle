0bcbb3a305fe8ca84461be8241534544d9deb335
Brecht Serckx <services+git@brechtserckx.be>
2022-11-28 20:41:04 +0100

    Initial encoding section

--- recycle-ics/frontend/src/section/encoding.tsx
+++ recycle-ics/frontend/src/section/encoding.tsx
@@ -1,3 +1,60 @@
 export default function EncodingSection() {
-  return <></>;
+  return (
+    <>
+      <h3>Encoding</h3>
+      <fieldset>
+        <legend>Fraction encoding</legend>
+        <label>
+          <input type="radio" name="fe" value="event" />
+          Event
+        </label>
+        <p>Represent waste collections as an event.</p>
+        <label>
+          Start time:
+          <input type="time" name="es" required value="07:00" disabled />
+        </label>
+        <label>
+          End time:
+          <input type="time" name="ee" required value="10:00" disabled />
+        </label>
+        <p>Reminders: </p>
+        <ul id="reminder_list"> </ul>
+        <button id="add_reminder_button" type="button">
+          Add reminder
+        </button>
+        <label>
+          <input type="radio" name="fe" value="todo" defaultChecked />
+          Todo
+        </label>
+        <p>
+          Represent waste collections as a todo or task.
+          <strong>Does not work with Google Calendar! </strong>Maybe with
+          Outlook, that's untested.
+        </p>
+        <fieldset>
+          <legend>Due type</legend>
+          <label>
+            <input type="radio" name="tdt" value="date" />
+            Full day:
+          </label>
+          <label>
+            Days before:
+            <input type="number" name="tdb" required value="1" disabled />
+          </label>
+          <label>
+            <input type="radio" name="tdt" value="datetime" />
+            Specific time:
+          </label>
+          <label>
+            Days before:
+            <input type="number" name="tdb" required value="1" disabled />
+          </label>
+          <label>
+            Time:
+            <input type="time" name="tt" required value="20:00" disabled />
+          </label>
+        </fieldset>
+      </fieldset>
+    </>
+  );
 }
