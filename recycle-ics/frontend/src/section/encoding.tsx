3f2b6aa97e00b05d161d6e43b081fb73eaefe63f
Brecht Serckx <services+git@brechtserckx.be>
2022-11-29 09:59:21 +0100

    Use `EncodingRadio`

--- recycle-ics/frontend/src/section/encoding.tsx
+++ recycle-ics/frontend/src/section/encoding.tsx
@@ -1,5 +1,16 @@
 import * as React from "react";
 
+const EncodingRadio = React.forwardRef(
+  ({ children, label, ...props }: any, ref: any) => (
+    <div>
+      <label>
+        <input ref={ref} type="radio" {...props} />
+        {label}:{children}
+      </label>
+    </div>
+  )
+);
+
 const EventTimeInput = React.forwardRef(
   ({ label, ...props }: any, ref: any) => (
     <label>
@@ -28,71 +39,63 @@ export default function EncodingSection() {
       <h3>Encoding</h3>
       <fieldset>
         <legend>Fraction encoding</legend>
-        <label>
-          <input type="radio" name="fe" value="event" />
-          Event
-        </label>
-        <p>Represent waste collections as an event.</p>
-        <EventTimeInput
-          label="Start time"
-          name="es"
-          required
-          value="07:00"
-          disabled
-        />
-        <EventTimeInput
-          label="End time"
-          name="ee"
-          required
-          value="10:00"
-          disabled
-        />
-        <p>Reminders: </p>
-        <ul id="reminder_list"> </ul>
-        <button id="add_reminder_button" type="button">
-          Add reminder
-        </button>
-        <label>
-          <input type="radio" name="fe" value="todo" defaultChecked />
-          Todo
-        </label>
-        <p>
-          Represent waste collections as a todo or task.
-          <strong>Does not work with Google Calendar! </strong>Maybe with
-          Outlook, that's untested.
-        </p>
-        <fieldset>
-          <legend>Due type</legend>
-          <label>
-            <input type="radio" name="tdt" value="date" />
-            Full day:
-          </label>
-          <TodoDaysBeforeInput
-            label="Days before"
-            name="tdb"
+        <EncodingRadio label="Event" name="fe" value="event">
+          <p>Represent waste collections as an event.</p>
+          <EventTimeInput
+            label="Start time"
+            name="es"
             required
-            value="1"
+            value="07:00"
             disabled
           />
-          <label>
-            <input type="radio" name="tdt" value="datetime" />
-            Specific time:
-          </label>
-          <TodoDaysBeforeInput
-            label="Days before"
-            name="tdb"
+          <EventTimeInput
+            label="End time"
+            name="ee"
             required
-            value="1"
+            value="10:00"
             disabled
           />
-          <TodoTimeInput
-            label="Time"
-            name="tt"
-            required
-            value="20:00"
-            disabled
-          />
-        </fieldset>
+          <p>Reminders: </p>
+          <ul id="reminder_list"> </ul>
+          <button id="add_reminder_button" type="button">
+            Add reminder
+          </button>
+        </EncodingRadio>
+        <EncodingRadio label="Todo" name="fe" value="todo">
+          <p>
+            Represent waste collections as a todo or task.
+            <strong>Does not work with Google Calendar! </strong>Maybe with
+            Outlook, that's untested.
+          </p>
+          <fieldset>
+            <legend>Due type</legend>
+            <EncodingRadio label="Full day" name="tdt" value="date">
+              <TodoDaysBeforeInput
+                label="Days before"
+                name="tdb"
+                required
+                value="1"
+                disabled
+              />
+            </EncodingRadio>
+            <EncodingRadio label="Specific time" name="tdt" value="datetime">
+              <TodoDaysBeforeInput
+                label="Days before"
+                name="tdb"
+                required
+                value="1"
+                disabled
+              />
+              <TodoTimeInput
+                label="Time"
+                name="tt"
+                required
+                value="20:00"
+                disabled
+              />
+            </EncodingRadio>
+          </fieldset>
+        </EncodingRadio>
       </fieldset>
     </>
   );
