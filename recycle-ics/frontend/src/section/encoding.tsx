b233e71959418b69ced7f634ffd02937ef16f2b7
Brecht Serckx <services+git@brechtserckx.be>
2023-01-02 17:43:19 +0100

    Small encoding fixes

--- recycle-ics/frontend/src/section/encoding.tsx
+++ recycle-ics/frontend/src/section/encoding.tsx
@@ -15,7 +15,7 @@ const EncodingRadio = React.forwardRef(
 const EventTimeInput = React.forwardRef(
   ({ label, ...props }: any, ref: any) => (
     <label>
-      {label} <input type="time" {...props} />
+      {label} <input ref={ref} type="time" {...props} />
     </label>
   )
 );
@@ -36,13 +36,13 @@ const EventInputs = () => {
       <p>Represent waste collections as an event.</p>
       <EventTimeInput
         label="Start time"
-        value="07:00"
+        defaultValue="07:00"
         disabled={!isChecked}
         {...register("es", { required: true })}
       />
       <EventTimeInput
         label="End time"
-        value="10:00"
+        defaultValue="10:00"
         disabled={!isChecked}
         {...register("ee", { required: true })}
       />
@@ -74,17 +74,18 @@ const TodoFullDayInputs = ({ isParentChecked }: any) => {
   const value = "date";
   const defaultChecked = true;
   var isChecked =
-    useWatch({ name: "drt", defaultValue: defaultChecked && value }) === value;
+    useWatch({ name: "fe", defaultValue: defaultChecked && value }) === value;
   return (
     <EncodingRadio
       label="Full day"
       value={value}
       disabled={!isParentChecked}
+      defaultChecked={defaultChecked}
       {...register("tdt")}
     >
       <TodoDaysBeforeInput
         label="Days before"
-        value="1"
+        defaultValue="1"
         disabled={!isChecked}
         {...register("tdb", { required: true })}
       />
@@ -97,7 +98,7 @@ const TodoSpecificTimeInputs = ({ isParentChecked }: any) => {
   const value = "datetime";
   const defaultChecked = false;
   var isChecked =
-    useWatch({ name: "drt", defaultValue: defaultChecked && value }) === value;
+    useWatch({ name: "fe", defaultValue: defaultChecked && value }) === value;
   return (
     <EncodingRadio
       label="Specific time"
@@ -108,13 +109,13 @@ const TodoSpecificTimeInputs = ({ isParentChecked }: any) => {
     >
       <TodoDaysBeforeInput
         label="Days before"
-        value="1"
+        defaultValue="1"
         disabled={!isChecked}
         {...register("tdb", { required: true })}
       />
       <TodoTimeInput
         label="Time"
-        value="20:00"
+        defaultValue="20:00"
         disabled={!isChecked}
         {...register("tt", { required: true })}
       />
@@ -129,7 +130,7 @@ const TodoInputs = () => {
   var isChecked =
     useWatch({ name: "fe", defaultValue: defaultChecked && value }) === value;
   return (
-    <EncodingRadio value={value} label="Todo:" {...register("fe")}>
+    <EncodingRadio value={value} label="Todo" {...register("fe")}>
       <p>
         Represent waste collections as a todo or task.
         <strong>Does not work with Google Calendar! </strong>Maybe with Outlook,
