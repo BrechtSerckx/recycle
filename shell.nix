d1dbca6244843cd822186940560d6a96fbda61d7
Brecht Serckx <services+git@brechtserckx.be>
2023-05-17 19:33:54 +0200

    Update act to run nix build with it

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 let
   inherit (import ./default.nix {}) sources nixpkgs hsPkgs;
   nixpkgs-node = import sources.nixpkgs-node {};
+  nixpkgs-act = import sources.nixpkgs-act {};
 in hsPkgs.shellFor {
   packages = ps: with ps; [ recycle-client recycle-ics ];
 
@@ -21,13 +22,14 @@ in hsPkgs.shellFor {
     nixfmt
     # haskell ci/cd generator
     haskell-ci
+    ]) ++ (with nixpkgs-act;  [
     # run ci/cd locally
     act
     ]) ++ (with nixpkgs-node;  [
     # nodejs for frontend
     nodejs
     prefetch-npm-deps
-  ]) ++ (with nixpkgs-node.nodePackages; [
+    ]) ++ (with nixpkgs-node.nodePackages; [
     # create-react-app
     create-react-app
     # package manager
